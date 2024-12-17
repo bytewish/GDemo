4f6ee68b6fdfbd934170b9c306c1b030afc80b8a
magician <2218590381@qq.com>
2024-12-17 16:27:25 +0800

    initialize the project

---
+++ glib/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
+}
+
+afterEvaluate{
+    android.libraryVariants.forEach{ variant ->
+        publishing.publications.create(variant.name,MavenPublication::class){
+            from(components.findByName(variant.name))
+            groupId = "com.gx"
+            artifactId = "glib"
+            version = "0.0.1"
+        }
+    }
+}
+
+android {
+    namespace = "com.gx.glib"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 19
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+}
